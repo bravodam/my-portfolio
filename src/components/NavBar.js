8e6544d6a4fe6113f3cc76bca919ef571f7fd04e
Bravo <ebenezerolushola8@gmail.com>
2021-02-23 07:55:15 +0100

    corrected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,8 +7,8 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
     return (
-        <header className="bg-red-600">
-            <div className="container mx-auto flex justify-between position:fixed">
+        <header className="bg-red-600 position:fixed">
+            <div className="container mx-auto flex justify-between ">
                 <nav className="flex">
                     <NavLink to="/" exact
                         activeClassName="text-white"
