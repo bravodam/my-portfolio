03872697f930ed073eceefab8a733201932634f5
Bravo <ebenezerolushola8@gmail.com>
2021-02-23 08:12:17 +0100

    corrected2

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     return (
         <main>
             <img src={ image} alt="food" className="absolute object-cover w-full h-full"/>
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-4">
-                <h1 className="text-4xl text-blue-800 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Ebenezer and I'm a software Developer.</h1>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-3xl text-red-800 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Ebenezer and I'm a software Developer.</h1>
             </section>
         </main>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,12 +7,13 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
     return (
-        <header className="bg-red-600 position:fixed">
-            <div className="container mx-auto flex justify-between ">
-                <nav className="flex">
-                    <NavLink to="/" exact
-                        activeClassName="text-white"
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+        <header className=" bg-red-800">
+            <div className="container mx-auto flex justify-between">
+            <nav className="flex">
+            
+                    <NavLink to="/" exact activeClassName="text-white"
+                        className="inflex-flex items center py-6 px-3 mr-4 text-red-200 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+
                         Ebenezer
                     </NavLink>
                     <NavLink to="/post"
@@ -20,11 +21,13 @@ export default function NavBar() {
                     activeClassName="text-red-100 bg-red-700">
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    <NavLink to="/project"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
                     activeClassName="text-red-100 bg-red-700">
                         Projects
                     </NavLink>
-                    <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    <NavLink to="/about"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
                     activeClassName="text-red-100 bg-red-700">
                         About Me! 
                     </NavLink>
@@ -38,6 +41,9 @@ export default function NavBar() {
                         className="mr-4 target=_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
                     <SocialIcon
                         url="https://www.linkedin.com/in/ebenezer-akinde-04a48019b/"
+                        className="mr-4 target=_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon
+                        url="https://www.twitter.com.com"
                         className="mr-4 target=_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
                 </div>
             </div>
